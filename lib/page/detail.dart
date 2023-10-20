46194252739f010afc95dde4d0c5ef436c0f1645
dragon5354 <dragon5354@naver.com>
2023-10-20 16:41:54 +0900

    slider 추가, (#8)

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -1,3 +1,7 @@
+/*
+* 홈 내부의 리스트를 눌렀을 때 세부 정보 페이지
+*/
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class DetailContentView extends StatefulWidget {
@@ -11,33 +15,110 @@ class DetailContentView extends StatefulWidget {
 class _DetailContentViewState extends State<DetailContentView> {
   // 휴대폰 기종에 맞춘 크기 받아오기
   Size? size;
+
+  // 이미지를 리스트로 만들어서 받아오는 용도
+  late List<String> imgList;
+
+  int? _current;
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     size = MediaQuery.of(context).size;
+    _current = 0;
+    imgList = [
+      // 실제로는 다른 이미지겠지만 이번은 그냥 같은 걸로 쓰기...
+      widget.data["image"].toString(),
+      widget.data["image"].toString(),
+      widget.data["image"].toString(),
+      widget.data["image"].toString(),
+      widget.data["image"].toString(),
+    ];
   }
 
   PreferredSizeWidget _appbarWidget() {
     return AppBar(
       backgroundColor: Colors.transparent, // 투명 앱바
       elevation: 0,
+      // 뒤로가기 임시 비활성화.
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Icon(Icons.arrow_back, color: Colors.white),
+      ),
       actions: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.share)),
-        IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        IconButton(
+            onPressed: () {}, icon: Icon(Icons.share, color: Colors.white)),
+        IconButton(
+            onPressed: () {}, icon: Icon(Icons.more_vert, color: Colors.white)),
       ],
     );
   }
 
   Widget _bodyWidget() {
     return Container(
-      child: Hero(
-        tag: widget.data["cid"].toString(),
-        child: Image.asset(
-          widget.data["image"]!,
-          fit: BoxFit.fill,
-          width: size!.width,
-        ),
+      child: Stack(
+        children: [
+          Hero(
+            tag: widget.data["cid"].toString(),
+            // 슬라이더
+            child: CarouselSlider(
+              options: CarouselOptions(
+                height: size!.width,
+                initialPage: 0,
+                enableInfiniteScroll: false,
+                viewportFraction: 1,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                  print(index);
+                },
+              ),
+              items: imgList.map((url) {
+                return Image.asset(
+                  url,
+                  width: size!.width,
+                  fit: BoxFit.fill,
+                );
+              }).toList(),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.asMap().entries.map((entry) {
+                return GestureDetector(
+                  // onTap: () => _controller.animateToPage(entry.key),
+                  child: Container(
+                    width: 12.0,
+                    height: 12.0,
+                    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: (Theme.of(context).brightness == Brightness.dark
+                                ? Colors.white
+                                : Colors.white.withOpacity(0.4))
+                            .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _bottomBarWidget() {
+    return Container(
+      width: size!.width,
+      height: 55,
+      color: Colors.red,
     );
   }
 
@@ -47,6 +128,7 @@ class _DetailContentViewState extends State<DetailContentView> {
       extendBodyBehindAppBar: true, // body 부분을 appbar 영역까지 쓰도록 허용
       appBar: _appbarWidget(),
       body: _bodyWidget(),
+      bottomNavigationBar: _bottomBarWidget(),
     );
   }
 }
