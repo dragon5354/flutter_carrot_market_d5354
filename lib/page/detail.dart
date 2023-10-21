4b6c7c2a1dc0a12086d0553af2925838f7268c2f
dragon5354 <dragon5354@naver.com>
2023-10-21 11:12:03 +0900

    세부정보의 제품설명, customScrollView 추가, (#10)

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -142,13 +142,131 @@ class _DetailContentViewState extends State<DetailContentView> {
     );
   }
 
+  // 경계선을 위젯으로 만들어줌
+  Widget _line() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      height: 1,
+      color: Colors.grey.withOpacity(0.3),
+    );
+  }
+
+  // 원래는 api를 통해 받아올 내용
+  // 그냥 영역만 잡고 일단 패스
+  Widget _contentDetail() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 20),
+          Text(
+            widget.data["title"].toString(),
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "디지털/가전 ∙ 22시간 전",
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(
+            "선물받은 새상품이고\n상품 꺼내보기만 했습니다\n거래는 직거래만 합니다.",
+            style: TextStyle(fontSize: 15, height: 1.5),
+          ),
+          SizedBox(height: 15),
+          Text(
+            "채팅 3 ∙ 관심 17 ∙ 조회 295",
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 15),
+        ],
+      ),
+    );
+  }
+
+  Widget _otherCellContents() {
+    return Padding(
+      padding: EdgeInsets.all(15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "판매자님의 판매 상품",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "모두보기",
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _bodyWidget() {
-    return Column(
-      children: [
+    // 리스트를 처리하기 위해선 singlechildscrollview 대신 custom을 써야 함
+    return CustomScrollView(slivers: [
+      SliverList(
+          delegate: SliverChildListDelegate([
         _makeSliderImage(),
         _sellerSimpleInfo(),
-      ],
-    );
+        _line(),
+        _contentDetail(),
+        _line(),
+        _otherCellContents(),
+      ])),
+      SliverPadding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        sliver: SliverGrid(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
+          delegate: SliverChildListDelegate(List.generate(20, (index) {
+            return Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      color: Colors.grey.withOpacity(0.3),
+                      height: 120,
+                    ),
+                  ),
+                  Text(
+                    "상품 제목",
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  Text(
+                    "금액",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            );
+          }).toList()),
+        ),
+      ),
+    ]);
   }
 
   Widget _bottomBarWidget() {
